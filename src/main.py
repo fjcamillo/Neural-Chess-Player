06b79c8280cc85438f73d0f7cc0f2ca83bc42256
fjcamillo <fjcamillo.dev@gmail.com>
2017-07-18 23:38:51 +0800

    added conv function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import tensorflow as tf
 import numpy as np
 
+def convolutional_layer(data, weight, strides, padding):
+    conv = tf.conv2d(data, weight, strides, padding)
+    
 
 def main():
-    pass
+    with tf.device('./cpu:0'):
+
 
 if __name__ == '__main__':
     main()
