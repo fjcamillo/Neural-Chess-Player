f69286e0b5c46922c42019df070c2196d66b2876
fjcamillo <fjcamillo.dev@gmail.com>
2017-08-06 17:52:04 +0800

    finished convolutional neural net

--- src/models/conv.py
+++ src/models/conv.py
@@ -3,10 +3,11 @@ import numpy as np
 import pandas as pd
 
 
-def cnn_model(device='/cpu:0'):
+def cnn_model(device='/cpu:0', epoch=1):
     with tf.device(device):
         x = tf.placeholder(tf.float32, [None, 48, 48, 3])
         y = tf.placeholder(tf.float32, [None, 12])
+        keep_prob = tf.placeholder(tf.float32)
 
         conv1_w = tf.Variable(tf.truncate_normal([8, 8, 3, 8]), tf.float32)
         conv1_b = tf.Variable(tf.zeros([8]))
@@ -34,4 +35,16 @@ def cnn_model(device='/cpu:0'):
         conv3 = tf.nn.relu(tf.nn.conv2d(max_2, conv3_w, stride[1, 1, 1, 1], padding='SAME'))
         max_3 = tf.nn.max_pool(conv3, kstrides=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-        
+        reshaped = tf.reshape(max_3, [-1, 48*48*32])
+
+        fc1 = tf.nn.dropout(tf.nn.relu(tf.matmul(reshaped, fc1_w) + fc1_b), keep_prob)
+        fc2 = tf.nn.dropout(tf.nn.relu(tf.matmul(fc1, fc2_w) + fc2_b), keep_prob)
+        fc3 = tf.matmul(fc2, tf3_w) + fc3_b
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=fc3))
+
+        train = tf.train.AdamOptimizer().minimize(cross_entropy)
+
+        cross_prediction = tf.equal(tf.argmax(fc3, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(cross_prediction, tf.float32))
+
+    sess = tf.InteractiveSession().run()
